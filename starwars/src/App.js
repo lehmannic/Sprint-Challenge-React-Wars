cc2326e966f635fa6b0daebe109f5d460a46f55d
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-05-15 12:54:04 -0500

    init commit

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'; 
+import {BASE_URL} from './constants'
+
 import './App.css';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [charactersInfo, setCharactersInfo] = useState([]); 
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+      .then(response => {
+        console.log(response)
+        console.log(response.data.info.count)
+        setCharactersInfo(response.data)
+      })
+      .catch(error => {
+        console.log(`could not fetch all characters`)
+        debugger
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <></>
+      <Character info={charactersInfo} />
+      
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,13 @@
 // Write your Character component here
+
+import React from "react"
+
+export default function Character({ info }) {
+    if (!info) return <h3> Gathering character info...</h3>
+    return (
+        <div className='characterContainer'>
+            <h3>{info.name}</h3>
+            <img src={info.image}></img>
+        </div>
+    )
+}
