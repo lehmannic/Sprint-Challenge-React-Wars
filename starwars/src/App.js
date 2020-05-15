b973d08096a450f401e5cffbcb2612288957bbf2
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-05-15 14:03:11 -0500

    simple styling added via styled-components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,15 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'; 
+import styled from 'styled-components'
 import {BASE_URL} from './constants'
 
 import './App.css';
 import Character from './components/Character'
 
+const StyledApp = styled.div`
+  /* display: flex; 
+  flex-wrap: wrap;  */
+`
+const StyledCharacterContainer = styled.div`
+  display: flex; 
+  justify-content: center; 
+  flex-wrap: wrap; 
+`
+
+
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [charactersInfo, setCharactersInfo] = useState([]); 
-  // console.log("App -> charactersInfo", charactersInfo)
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -26,9 +37,9 @@ const App = () => {
   }, [])
 
   return (
-    <div className="App">
+    <StyledApp className="App">
       <h1 className="Header">Characters</h1>
-      <div>
+      <StyledCharacterContainer>
         {
           charactersInfo.map((character) => {
             return (
@@ -36,9 +47,9 @@ const App = () => {
             )
           })
         }
-      </div>
+      </StyledCharacterContainer>
 
-    </div>
+    </StyledApp>
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,13 +1,28 @@
 // Write your Character component here
 import React from "react"
+import styled from "styled-components"
+
+const StyledCharacter = styled.div`
+    width: 22%; 
+    border: 1px solid rgb(210, 210, 210);
+    box-shadow: 0px 1px 6px -2px rgb(128, 127, 127);
+    border-radius: 8px;
+    margin: 16px;
+    padding: 16px 8px 12px 16px;
+    background-color: white;
+
+    img {
+        width: 75%; 
+    }
+`
 
 export default function Character({ info }) {
     console.log(info)
     if (!info) return <h3> Gathering character info...</h3>
     return (
-        <div className='characterContainer'>
+        <StyledCharacter className='characterContainer'>
             <h3>{info.name}</h3>
             <img src={info.image}></img>
-        </div>
+        </StyledCharacter>
     )
 }
